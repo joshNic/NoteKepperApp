7ebefe2d1cefec742e0b00d13c8e4c3f82075515
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-03-25 14:10:08 +0300

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+h
+8app/src/main/java/com/example/notekeeper/MainActivity.kt,d/3/d3fe8d072cc30b815d77bde47865e942722ac033
---
+++ app/src/main/java/com/example/notekeeper/dataManagers/DataManager.kt
@@ -0,0 +1,27 @@
+package com.example.notekeeper.dataManagers
+
+import com.example.notekeeper.models.CourseInfo
+import com.example.notekeeper.models.NoteInfo
+
+class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses(){
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+    }
+}
